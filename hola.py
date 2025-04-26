451bd7a39147314ca3b3a7d1eee4d240c44cf138
Harold <galeanoalzateharoldandres@gmail.com>
2025-04-26 12:51:17 -0500

    primer cambio

---
+++ hola.py
@@ -0,0 +1,13 @@
+print("hola mundo")
+print(2)
+numero = 2
+numero = 5
+numero_1 = "3"
+numero_2 = "5"
+prueba = numero_1*5#aca se multiplica numero_1 por 5
+texto = "soy programador"
+print(type(prueba))
+print(prueba) 
+print(texto)
+print(type(numero))
+""""esto es un comentario de varias lineas"""
