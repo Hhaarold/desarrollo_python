d38ca409dfd65410516730c26e2c298a26132bd8
harold <galeanoalzateharoldandres@gmail.com>
2025-05-10 10:30:16 -0500

    creando sets

---
+++ sets/sets.py
@@ -0,0 +1,11 @@
+sets1 = {1,2,3,4,5}#creacion de sets
+print(type(sets1))
+print(sets1)
+sets1.add(6)#agregar elementos
+print(sets1)
+sets1.remove(3)#eliminar elementos
+print(sets1)
+sets1.discard(2)
+print(sets1)
+sets1.clear()#vaciar set
+print(sets1)
